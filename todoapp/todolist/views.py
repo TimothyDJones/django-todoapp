11e07cc38af07af96dcdf8c1f50b6136028f82d3
Tim Jones <tdjones74021@yahoo.com>
2020-11-10 16:04:54 -0600

    Add form to add new task.

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import TaskForm
 
 # Create your views here.
 
 def index(request):
     #return HttpResponse("Hello, World!")
-    return render(request, "index.html")
+    form = TaskForm()
+
+    if request.method == 'POST':
+        # get the posted form data
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+
+    return render(request, "index.html", {"task_form": form})
