ddcb2cdb388b6edfca76bbed87af27b4869aa000
Tim Jones <tdjones74021@yahoo.com>
2020-11-10 20:14:13 -0600

    Add Bootstrap styling to 'forms.py' via Django Forms attributes.

--- todoapp/todolist/forms.py
+++ todoapp/todolist/forms.py
@@ -7,3 +7,24 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = "__all__"  # include all fields
         # fields = ('title', 'completed')
+
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter task description...",
+            }
+        ),
+    )
+
+    completed = forms.CharField(
+        required=False,
+        widget=forms.widgets.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+            }
+        ),
+    )
+
+# For more about Django Forms, see:
+# https://www.webforefront.com/django/formfieldtypesandvalidation.html
